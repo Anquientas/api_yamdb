42c9eb5b1eeb0164bfe816bc6b8210905f6e4709
Elena Altman <altmanhellen@gmail.com>
2024-02-16 09:41:49 +0200

    Add import_data for import from csv to BD.

--- api_yamdb/import_data.py
+++ api_yamdb/import_data.py
@@ -3,6 +3,24 @@ from reviews.models import Category, Comment, Genre, Title, Review
 from users.models import User
 
 
+def import_users():
+    with open('static/data/users.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if User.objects.filter(email=row['email']).exists():
+                print(f"Пользователь с email {row['email']} уже существует")
+                continue
+            User.objects.create(
+                id=row['id'],
+                username=row['username'],
+                email=row['email'],
+                role=row['role'],
+                bio=row.get('bio',),
+                first_name=row.get('first_name',),
+                last_name=row.get('last_name',)
+            )
+
+
 def import_category():
     with open('static/data/category.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -10,32 +28,6 @@ def import_category():
             slug = row['slug']
             if not Category.objects.filter(slug=slug).exists():
                 Category.objects.create(name=row['name'], slug=slug)
-
-
-def import_comment():
-    with open('static/data/comments.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            try:
-                review = Review.objects.get(id=row['review_id'])
-            except Review.DoesNotExist:
-                print(f"Обзор с ID {row['review_id']} не найден")
-                continue
-            Comment.objects.create(
-                text=row['text'],
-                author=row['author'],
-                pub_date=row['pub_date'],
-                review=review
-            )
-
-
-def import_genretitle():
-    with open('static/data/genre_title.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)  # DictReader возвращает словарь из строки csv файла (ключи - названия колонок)
-        for row in reader:
-            title = Title.objects.get(id=row['title_id'])
-            genre = Genre.objects.get(id=row['genre_id'])
-            title.genre.add(genre)  # используем add, чтобы не создавать дубликаты
 
 
 def import_genre():
@@ -53,6 +45,7 @@ def import_title():
         for row in reader:
             category, _ = Category.objects.get_or_create(id=row['category'])
             Title.objects.create(
+                id=row['id'],
                 name=row['name'],
                 year=row['year'],
                 category=category
@@ -63,30 +56,45 @@ def import_review():
     with open('static/data/review.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
+            author = User.objects.get(id=row['author'])
             title = Title.objects.get(id=row['title_id'])
             Review.objects.create(
+                id=row['id'],
                 text=row['text'],
-                author=row['author'],
+                author=author,
                 score=row['score'],
                 pub_date=row['pub_date'],
                 title=title
             )
 
 
-def import_users():
-    with open('static/data/users.csv', newline='') as csvfile:
+def import_genretitle():
+    with open('static/data/genre_title.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            if User.objects.filter(email=row['email']).exists():
-                print(f"Пользователь с email {row['email']} уже существует")
+            if not Title.objects.filter(id=row['title_id']).exists():
+                print(f"Произведение с ID {row['title_id']} не найдено")
                 continue
-            User.objects.create(
-                username=row['username'],
-                email=row['email'],
-                role=row['role'],
-                bio=row.get('bio',),
-                first_name=row.get('first_name',),
-                last_name=row.get('last_name',)
+            title = Title.objects.get(id=row['title_id'])
+            genre = Genre.objects.get(id=row['genre_id'])
+            title.genre.add(genre)  # используем add, чтобы не создавать дубликаты
+
+
+def import_comment():
+    with open('static/data/comments.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            author = User.objects.get(id=row['author'])
+            review = Review.objects.get(id=row['review_id'])
+            if not Review.objects.filter(id=row['review_id']).exists():
+                print(f"Ревью с ID {row['review_id']} не найдено")
+                continue
+            Comment.objects.create(
+                id=row['id'],
+                review=review,
+                text=row['text'],
+                author=author,
+                pub_date=row['pub_date']
             )
 
 
